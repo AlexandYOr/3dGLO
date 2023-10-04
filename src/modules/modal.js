45a5ac886e725e3f2444dc6135a5c0da3eb1c8af
AlexandYOr <avo01121997@yandex.ru>
2023-10-04 21:36:06 +0300

    create animation for modal | lesson19

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,16 +1,41 @@
 const modal = () => {
     const buttons = document.querySelectorAll('.popup-btn')
     const modal = document.querySelector('.popup')
-    const closeBtn = document.querySelector('.popup-close')
+    const closeBtn = modal.querySelector('.popup-close')
+    const modalContent = modal.querySelector('.popup-content')
+    console.dir(window.screen)
+    let count = -30
+    const animationModal = () => {
+        count++
+        if (count <= 38) {
+            modalContent.style.left = `${count}%`
+            requestAnimationFrame(animationModal)
+        }
+    }
+    const reverseAnimationModal = () => {
+        count--
+        if (count > -30) {
+            modalContent.style.left = `${count}%`
+            requestAnimationFrame(reverseAnimationModal)
+        } else if (count === -30) {
+            modal.style.display = 'none'
+        }
+    }
 
     buttons.forEach(btn => {
         btn.addEventListener('click', () => {
             modal.style.display = 'block'
+            if (document.documentElement.clientWidth > 768) {
+                requestAnimationFrame(animationModal) 
+            }   
         })
     })
-
     closeBtn.addEventListener('click', () => {
-        modal.style.display = 'none'
+        if (document.documentElement.clientWidth > 768) {
+            requestAnimationFrame(reverseAnimationModal)
+        } else {
+            modal.style.display = 'none'
+        }
     })
 }
 
