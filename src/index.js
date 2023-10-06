7b899900ab662a9931d1b3b73b87ee2a9f607c9e
AlexandYOr <avo01121997@yandex.ru>
2023-10-06 13:17:35 +0300

    create validation | lesson 20

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import bootstrap from "../css/bootstrap.min.css"
 import style from "../css/style.min.css" 
 import menu from "./modules/menu.js"
 import modal from "./modules/modal.js"
+import validation from "./modules/validation.js"
 
 timer('05 october 2023')
 menu()
 modal()
+validation()
